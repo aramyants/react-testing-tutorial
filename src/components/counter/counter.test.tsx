b759976ca5b71c6626e397d5baf16847093f300d
aramyants <aramaramyants@gmail.com>
2026-03-29 20:59:49 +0400

    feat: add user-event setup and enhance Counter component tests

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import { Counter } from "./counter";
 
 describe("Counter", () => {
@@ -11,5 +12,21 @@ describe("Counter", () => {
     const buttonElement = screen.getByRole("button", { name: /Increment/i });
     expect(buttonElement).toBeInTheDocument();
   });
-});
 
+  test('renders a count of 0', () => {
+    render(<Counter />);
+
+    const headingElement = screen.getByRole("heading", { name: /You clicked 0 times/i });
+    expect(headingElement).toHaveTextContent("You clicked 0 times");
+  });
+
+  test('increments the count when the button is clicked', async () => {
+    user.setup();
+    render(<Counter />);
+    const buttonElement = screen.getByRole("button", { name: /Increment/i });
+    await user.click(buttonElement);
+
+    const headingElement = screen.getByRole("heading", { name: /You clicked 1 times/i });
+    expect(headingElement).toHaveTextContent("You clicked 1 times");
+  });
+});
